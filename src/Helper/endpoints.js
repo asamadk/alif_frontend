8cd488d08855f6fa03937abecb3814b6947b77cd
Abdul Samad <abdul.samadkirmani.samad63@gmail.com>
2022-08-27 01:47:02 +0530

    Mail details

--- src/Helper/endpoints.js
+++ src/Helper/endpoints.js
@@ -1,5 +1,5 @@
-// const BASE_URL = 'https://alif-ecommerce.herokuapp.com/';
-const BASE_URL = 'http://localhost:8080/';
+const BASE_URL = 'https://alif-ecommerce.herokuapp.com/';
+// const BASE_URL = 'http://localhost:8080/';
 
 export const MAIL_ORDER = BASE_URL + 'mail/order/details/'
 
